9b0351bdf3f5793e29f77b022edd26f44102cc7f
SheepWaitForWolf <dglennie89@gmail.com>
2016-11-01 11:28:53 +0000

    auth update
    changing from portal to business

--- config/auth.php
+++ config/auth.php
@@ -72,7 +72,7 @@ return [
 
         'users' => [
             'driver' => 'database',
-            'table' => 'portal_users_mst',
+            'table' => 'business_users_mst',
         ],
     ],
 
