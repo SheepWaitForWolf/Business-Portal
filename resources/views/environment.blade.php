7b6140f32451e561efd924fe3c2d19d024afe831
SheepWaitForWolf <dglennie89@gmail.com>
2016-11-03 10:52:44 +0000

    environmental health blade update
    Tidied up content and formatting of environmental health blade

--- resources/views/environment.blade.php
+++ resources/views/environment.blade.php
@@ -7,23 +7,25 @@ Environmental Health
 @section('Content')
 <br>
 <div class="container">
+	<h3>Environmental Health </h3>
 	<div class="col-md-8">
-		 <p> The Council is responsible for enforcing food safety legislation within East Dunbartonshire boundaries to ensure that all food sold or produced is fit for human consumption and meets legal standards.  Duties include:
+		 <p> The Council is responsible for enforcing food safety legislation within East Dunbartonshire boundaries to ensure that all food sold or produced is fit for human consumption and meets legal standards.  Duties include: </p>
+<ul>
+<li>carry out programmed food hygiene inspections of all food businesses </li>
+<li>carry out food standards inspections of manufacturing and non-manufacturing premises </li>
+<li>investigate food complaints from residents and businesses in East Dunbartonshire and also complaints arising from foods manufactured in the area </li>
+<li>carry out surveillance of foods sold or produced in the area to ensure compliance against food safety and food quality standards </li>
+<li>promote food hygiene and safety to community groups within the East Dunbartonshire area</li>
 
-carry out programmed food hygiene inspections of all food businesses
-carry out food standards inspections of manufacturing and non-manufacturing premises
-investigate food complaints from residents and businesses in East Dunbartonshire and also complaints arising from foods manufactured in the area
-carry out surveillance of foods sold or produced in the area to ensure compliance against food safety and food quality standards
-promote food hygiene and safety to community groups within the East Dunbartonshire area
-Details on the policies and principles we apply are available in the document section.
+<p>Details on the policies and principles we apply are available in the document section.</p>
 
-If you wish to obtain further information, advice on food related topics, want to make a complaint about a local restaurant or food selling business, or want to start a food selling business please contact an Enforcement Officer through Customer Services (link sends e-mail).
+<p>If you wish to obtain further information, advice on food related topics, want to make a complaint about a local restaurant or food selling business, or want to start a food selling business please contact an Enforcement Officer through Customer Services (link sends e-mail).</p>
 
-Cross contamination prevention
+<p> Cross contamination prevention </p>
 
-The Food Standards Agency has produced guidance on preventing cross contamination. The guidance states that the safest and most reliable way to prevent cross contamination from raw foods to ready-to-eat food is by introducing complete physical separation. This would mean having completely separate equipment, food preparation areas and storage.
+<p>The Food Standards Agency has produced guidance on preventing cross contamination. The guidance states that the safest and most reliable way to prevent cross contamination from raw foods to ready-to-eat food is by introducing complete physical separation. This would mean having completely separate equipment, food preparation areas and storage.</p>
 
-The Council has produced a booklet based on the text of the Food Standards Agency’s CookSafe manual to inform Food Business Operators (FBOs) of the ways in which they can help to keep their customers free from food poisoning. It also explains changes to the way in which food hygiene inspections will be carried out and how the current law will be enforced.  This is available from the document section. </p>
+<p>The Council has produced a booklet based on the text of the Food Standards Agency’s CookSafe manual to inform Food Business Operators (FBOs) of the ways in which they can help to keep their customers free from food poisoning. It also explains changes to the way in which food hygiene inspections will be carried out and how the current law will be enforced.  This is available from the document section. </p>
 	</div>
 </div>
 @stop
