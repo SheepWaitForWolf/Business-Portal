5054a919fdc9cb815d9eff1ddd10428364617d22
SheepWaitForWolf <dglennie89@gmail.com>
2016-11-07 09:46:32 +0000

    window cleaner blade added
    view for window cleaner license application added along with page
    content

---
+++ resources/views/windowcleaner.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.master')
+
+@section('Title')
+Infographs
+@stop
+
+@section('Content')
+<br>
+<div class="container">
+	<div class="col-md-8">
+		 <p>To operate as a window cleaner in the East Dunbartonshire area you will need a licence from the Council, which is responsible for the processing of all licence applications including Window Cleaners.
+
+Eligibility Criteria
+
+You must not be disqualified from holding a licence, and you must be fit and proper person to be the holder of the licence.
+You must not have been refused a grant or renewal of the same licence within the last year, unless there has been a material change in your circumstances since your last application.
+Regulation summary
+
+A summary of the regulation relating to this licence (link is external)
+
+Payment 
+
+A licence fee of £147 for 3 years is payable.
+
+Applications must:
+
+be in writing (including by electronic means)
+be signed by the applicant (including by electronic means)
+contain the applicant's name and address, and those of any employee who will manage the activity on a day-to-day basis, and the area in which the activity will take place.
+The licensing authority will:
+
+Keep a register of applications in which they will enter the details of the receipt of the application, their final decision and reasons for the decision, the details of the terms of each licence granted and a note of any suspension, variation of the terms, or surrender, of a licence The register must be available for inspection by any member of the public at reasonable times and places and any member of the public must be permitted to make a copy of it.
+
+Application evaluation process
+
+There will be consultation with the local Chief Constable and other relevant consultees. There may be a hearing at which you may make representation.
+
+Will tacit consent apply?
+
+Yes. This means that you will be able to act as though your application is granted if you have not heard from the licensing authority by the end of the 130 day period permitted for final determination of the application.
+
+Apply for a Window Cleaner's Licence
+
+There are two ways of applying:
+
+download an application form to be completed and submitted to address on the form
+Window Cleaner's Licence application - apply online (link is external)
+Failed application redress
+
+Within 28 days of the date of the decision to refuse an application, the applicant may require the licensing authority to give the reasons for their decision.
+
+The applicant may appeal to the sheriff against the decision, within 28 days of the decision, as long as they have already followed any available procedure in terms of stating their case to the licensing authority.
+
+The appeal will only be successful if the sheriff considers that the licensing authority, in making their decision, had:
+
+erred in law
+based their decision on an incorrect material fact
+acted contrary to natural justice
+exercised their discretion in an unreasonable manner
+The applicant may then appeal on a point of law from the sheriff's decision to the Court of Session within 28 days from the date of the sheriff’s decision.
+
+Licence holder redress
+
+If the licensing authority decides to suspend a licence, not to renew a licence, or not to consent to material changes to the premises or vehicle used in the course of a licence holder's work, the licence holder may appeal against the decision within 28 days of being notified, as long as they have already followed any available procedure in terms of stating their case to the licensing authority.
+
+The appeal will only be successful if the sheriff considers that the licensing authority, in making their decision, had:
+
+erred in law
+based their decision on an incorrect material fact
+acted contrary to natural justice
+exercised their discretion in an unreasonable manner
+The licence holder may then appeal on a point of law from the sheriff's decision to the Court of Session within 28 days from the date of the sheriff's decision.
+ 
+
+Other redress
+
+Objections or representations relating to a licence application may be made in writing to the licensing authority, within 21 days of notice of the licence application being given, stating:
+
+the grounds of the objection or nature of the representation
+the name and address of the person making the representation
+A Chief Constable, or anyone who has made a relevant objection or representation regarding the licence, may appeal against a decision within 28 days of being notified, as long as they have already followed any available procedure in terms of stating their case to the licensing authority.
+
+The appeal will only be successful if the sheriff considers that the licensing authority, in making their decision, had:
+
+erred in law
+based their decision on an incorrect material fact
+acted contrary to natural justice
+exercised their discretion in an unreasonable manner
+Application to change a Window Cleaner's Licence
+
+If you wish to make a change to your existing licence, please contact the Licensing Team.</p>
+
+		</div>
+</div>
+@stop
