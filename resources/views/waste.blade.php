ecaa89d596c8f90c30f90e83d39ee6090f449513
SheepWaitForWolf <dglennie89@gmail.com>
2016-11-07 11:36:17 +0000

    waste blade update
    Formatted page content, added table and paragraphing

--- resources/views/waste.blade.php
+++ resources/views/waste.blade.php
@@ -10,41 +10,49 @@ Commercial Waste
 <br>
 <div class="container">
 	<div class="col-md-8">
-		<p> East Dunbartonshire Council will collect waste and recycling seven days a week, rather than just Monday to Friday. You should also be aware that your food waste may now be collected on a different day from your recycling or general waste.
+		<p> East Dunbartonshire Council will collect waste and recycling seven days a week, rather than just Monday to Friday. You should also be aware that your food waste may now be collected on a different day from your recycling or general waste.</p>
 
-The Council collects household waste in the following bins:
+<p>The Council collects household waste in the following bins:</p>
 
-Grey food caddy (collected weekly)	All food waste, cooked or raw, including bones and shells.  Please do not put liquids in this bin.
-Blue bin (collected fortnightly)	Paper, newspapers, cardboard, junk mail, magazines, catalogues, telephone directories.  Please make sure paper and card is clean and dry.
-Brown bin (collected fortnightly)	Glass jars and bottles, aerosol cans, food and drink cans, household plastic bottles (e.g. milk and juice containers, cleaning product bottles, shampoo and toiletry bottles).  Please wash and squash your bottles so they take up less space.
-Grey bin (collected fortnightly)	All other household waste (rubbish) that cannot be recycled. View our list of Frequently Asked Questions.
-Green bin (collected fortnightly March to November)	Garden waste e.g. flowers, plants, grass cuttings, hedge clippings, leaves, shrub pruning, weeds (not invasive plants like Japanese Knotweed), small amounts of shredded paper, twigs, small branches, sawdust and wood shavings, straw or animal bedding.  Please do not put soil, turf or rubble, food waste, animal faeces, large branches, garden furniture, broken garden ornaments and general waste in this bin.
-Compost your garden waste
+<table style="width: 100%" border="1" cellpadding="1" cellspacing="1"><tbody><tr><td style="width: 25%;"><strong>Grey food caddy </strong><strong>(collected weekly)</strong></td>
+<td>All food waste, cooked or raw, including bones and shells.  Please do not put liquids in this bin.</td>
+</tr><tr><td><strong>Blue bin (collected fortnightly)</strong></td>
+<td>Paper, newspapers, cardboard, junk mail, magazines, catalogues, telephone directories.  Please make sure paper and card is clean and dry.</td>
+</tr><tr><td><strong>Brown bin (collected fortnightly)</strong></td>
+<td>Glass jars and bottles, aerosol cans, food and drink cans, household plastic bottles (e.g. milk and juice containers, cleaning product bottles, shampoo and toiletry bottles).  Please wash and squash your bottles so they take up less space.</td>
+</tr><tr><td><strong>Grey bin (collected fortnightly)</strong></td>
+<td>All other household waste (rubbish) that cannot be recycled. View our list of <a href="/general-waste-bin-charge-faqs" target="_blank">Frequently Asked Questions.</a></td>
+</tr><tr><td><strong>Green bin (collected fortnightly March to November)</strong></td>
+<td>Garden waste e.g. flowers, plants, grass cuttings, hedge clippings, leaves, shrub pruning, weeds (<strong>not </strong>invasive plants like Japanese Knotweed), small amounts of shredded paper, twigs, small branches, sawdust and wood shavings, straw or animal bedding.  Please do not put soil, turf or rubble, food waste, animal faeces, large branches, garden furniture, broken garden ornaments and general waste in this bin.</td>
+</tr></tbody></table>
+
+<h3>Compost your garden waste</h3>
 
 Full information on home composting can be found at Zero Waste Scotland Home Composting (link is external).  You can buy composters at garden centres, supermarkets and online. 
 
-Collection of Household Waste
+<h3>Collection of Household Waste</h3>
 
 To make sure your bin is emptied, place it at the kerbside or collection point by 7am.  To find your collection day, please view the document section.
 
-Report a Missed Bin
+<h3>Report a Missed Bin</h3>
 
-Before reporting a missed bin please ensure that:
+<p>Before reporting a missed bin please ensure that:</p>
+<ul>
+<li>your bin was on the kerbside from 7am on your scheduled collection day</li>
+<li>you have put your bin out on the right day - check your collection day here</li>
+<li>the lid is fully closed</li>
+<li>if it is a recycling bin, that the correct items are in it</li>
+<li>access to the bin is not blocked by roadworks or parked cars.</li>
+<li>if missed please fill in the missed bin form.</li>
+</ul>
 
-your bin was on the kerbside from 7am on your scheduled collection day
-you have put your bin out on the right day - check your collection day here
-the lid is fully closed
-if it is a recycling bin, that the correct items are in it
-access to the bin is not blocked by roadworks or parked cars.
-if missed please fill in the missed bin form.
+<h3>Replacement bins</h3>
 
-Replacement bins
+<p>There is a charge of £19.42 for your first or a replacement grey general waste bin.  For replacement bins please email (link sends e-mail) or telephone Customer Services on 0300 123 4510.</p>
 
-There is a charge of £19.42 for your first or a replacement grey general waste bin.  For replacement bins please email (link sends e-mail) or telephone Customer Services on 0300 123 4510.
+<h3>Recycling Energy Saving Light Bulbs</h3>
 
-Recycling Energy Saving Light Bulbs
-
-You can recycle your energy saving light bulbs at our Mavis Valley Recycling Centre. Please tell a member of staff when you arrive that you would like to recycle light bulbs.</p>
+<p>You can recycle your energy saving light bulbs at our Mavis Valley Recycling Centre. Please tell a member of staff when you arrive that you would like to recycle light bulbs.</p>
 	</div>
 </div>
 @stop
