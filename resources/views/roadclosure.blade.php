451d307ce03c763b808708ac6d895460305e6645
SheepWaitForWolf <dglennie89@gmail.com>
2016-11-07 11:18:18 +0000

    road closure blade update
    Fixed formatting and layout using lists and paragraphing

--- resources/views/roadclosure.blade.php
+++ resources/views/roadclosure.blade.php
@@ -8,132 +8,138 @@ Infographs
 <br>
 <div class="container">
 	<div class="col-md-8">
-		 <p> Application to temporarily close a road
+
+			<div class="col-md-12">
+			<h2> Temporary Road Closure </h2>
+			<br>
+		</div>
+
+		 <p> Application to temporarily close a road </p>
 
 
-Licence summary
+<h3> Licence summary</h3>
 
 
-If you wish to temporarily close a road in East Dunbartonshire you must have consent from East Dunbartonshire Council. This temporary road closure procedure is for businesses, residents, charitable and other organisations, who may wish to apply for a temporary road closure or temporary parking restriction. 
+<p> If you wish to temporarily close a road in East Dunbartonshire you must have consent from East Dunbartonshire Council. This temporary road closure procedure is for businesses, residents, charitable and other organisations, who may wish to apply for a temporary road closure or temporary parking restriction. </p>
 
 
-Fee
+<h3> Fee </h3>
 
 
-If you plan to close a road for less than five days the fee is £226.00 for a temporary closure by notice. At least one weeks notice is required for the paperwork to be prepared.
+<p> If you plan to close a road for less than five days the fee is £226.00 for a temporary closure by notice. At least one weeks notice is required for the paperwork to be prepared.</p>
 
 
-If you plan to close the road for more than five days the fee is £396.00 for a temporary closure by order. At least four weeks notice is required for the paperwork to be prepared through Legal Services.
+<p>If you plan to close the road for more than five days the fee is £396.00 for a temporary closure by order. At least four weeks notice is required for the paperwork to be prepared through Legal Services.</p>
 
-Please note if you require a temporary road closure for example for a proposed event then the fee will be £396.00. At least four weeks notice is required for the paperwork to be prepared through Legal Services.
-
-
-Please contact Roads and Neighbourhood Services on 0300 1234510 or email Roads Permits (link sends e-mail) for further information. Alternatively write to us at the address shown below.
+<p>Please note if you require a temporary road closure for example for a proposed event then the fee will be £396.00. At least four weeks notice is required for the paperwork to be prepared through Legal Services.</p>
 
 
-Eligibility criteria
+<p>Please contact Roads and Neighbourhood Services on 0300 1234510 or email Roads Permits (link sends e-mail) for further information. Alternatively write to us at the address shown below.</p>
 
 
-A summary of the eligibility criteria (link is external) for this licence.
+<h3>Eligibility criteria</h3>
 
 
-Regulation evaluation process
+<p>A summary of the eligibility criteria (link is external) for this licence.</p>
 
 
-The Roads (Scotland) Act 1984.
+<h3>Regulation evaluation process</h3>
 
 
-Application evaluation process
+<p>The Roads (Scotland) Act 1984.</p>
+
+
+<h3>Application evaluation process</h3>
 
 
 Conditions may be attached to a consent
 
 
-Will tacit consent apply?
+<h3>Will tacit consent apply?</h3>
+
+<p>No. It is in the public interest that the Council must process your application before it can be granted. If you have not heard from us within a reasonable period, please contact us. You can do this online here if you applied through the UK Welcomes service or use the contact details below</p>
 
 
-No. It is in the public interest that the Council must process your application before it can be granted. If you have not heard from us within a reasonable period, please contact us. You can do this online here if you applied through the UK Welcomes service or use the contact details below
-
-
-Application to temporarily close a road
+<h3>Application to temporarily close a road</h3>
 
 
 There are two ways of applying:
 
+<uL>
+
+<li>contact East Dunbartonshire Council using the details provided.</li>
+
+<li>apply online using the appropriate link.</li>
+</uL>
+
+<h3>Application to temporarily close a road apply online (link is external)</h3>
 
 
-contact East Dunbartonshire Council using the details provided.
+<p>Roads Services </p>
 
-apply online using the appropriate link.
+<p>East Dunbartonshire Council Headquarters</p>
 
-Application to temporarily close a road apply online (link is external)
+<p>Southbank Marina</p>
 
+<p>12 Strathkelvin Place</p>
 
-Roads Services 
+<p>Kirkintilloch</p>
 
-East Dunbartonshire Council Headquarters
+<p>Glasgow</p>
 
-Southbank Marina
+<p>G66 1TJ</p>
 
-12 Strathkelvin Place
-
-Kirkintilloch
-
-Glasgow
-
-G66 1TJ
-
-Tel: 0300 1234510
-Email: Road Services (link sends e-mail)
+<p>Tel: 0300 1234510</p>
+<p>Email: Road Services (link sends e-mail)</p>
 
 
-Application redress
+<h3>Application redress</h3>
 
 
-Please contact the Neighbourhood Services Manager in the first instance.
+<p>Please contact the Neighbourhood Services Manager in the first instance.</p>
 
 
-Other redress
+<h3>Other redress</h3>
 
 
-Please contact the Neighbourhood Services Manager in the first instance about noise, pollution, etc.
+<p>Please contact the Neighbourhood Services Manager in the first instance about noise, pollution, etc.</p>
  
 
 
-Applying for a Temporary Road Closure or Temporary Parking Restriction
+<h3>Applying for a Temporary Road Closure or Temporary Parking Restriction</h3>
 
 
-Terms of Application
+<h3>Terms of Application</h3>
 
 
-This procedure is for businesses, charitable organisations and residents who wish to apply for a Temporary Order for road closure / parking restrictions / or similar. This facility may be granted to enable works on or adjacent to the road to be carried out in safety or for a charitable or community event to take place on the road. Temporary orders will not be permitted in order to allow contractors the use of the road for the purposes of site compounds.
+<p>This procedure is for businesses, charitable organisations and residents who wish to apply for a Temporary Order for road closure / parking restrictions / or similar. This facility may be granted to enable works on or adjacent to the road to be carried out in safety or for a charitable or community event to take place on the road. Temporary orders will not be permitted in order to allow contractors the use of the road for the purposes of site compounds.</p>
 
 
-Temporary Restriction durations and fees:
+<h3>Temporary Restriction durations and fees:</h3>
 
 
-1-5 days inclusive – Notice of Closure (£226.00 fee) Order of Closure (£396.00 fee) NB: For some charitable or community events the fee may be waived, however, applicants must comply with all other requirements.
+<p>1-5 days inclusive – Notice of Closure (£226.00 fee) Order of Closure (£396.00 fee) NB: For some charitable or community events the fee may be waived, however, applicants must comply with all other requirements.</p>
 
 
-Application period and notice required
+<h3>Application period and notice required</h3>
 
 
-All initial application forms and fees must be received at a minimum of 28 days before the required date of commencement of the restriction. In addition, all of the supporting information must be received and all details must be agreed and approved at a minimum of 21 days before the required date of commencement of the restriction.  Applicants are advised to take account of the time required to agree, organise and implement diversion schemes where these are necessary.
+<p>All initial application forms and fees must be received at a minimum of 28 days before the required date of commencement of the restriction. In addition, all of the supporting information must be received and all details must be agreed and approved at a minimum of 21 days before the required date of commencement of the restriction.  Applicants are advised to take account of the time required to agree, organise and implement diversion schemes where these are necessary.</p>
 
 
-Emergency road closures
+<h3>Emergency road closures</h3>
 
 
-Emergency closures are only permitted where there is a present or imminent danger to persons or property. In case of emergency, please telephone 0300 123 4510 immediately.
+<p>Emergency closures are only permitted where there is a present or imminent danger to persons or property. In case of emergency, please telephone 0300 123 4510 immediately.</p>
 
 
-Applicant’s responsibility
+<h3>Applicant’s responsibility</h3>
 
 
-It is the applicant’s responsibility to arrange, install, maintain and meet the cost of all on-site signing (including the diversion route where applicable; see Diversion Routes below) in accordance with Chapter 8 of the Traffic Signs Manual and / or Traffic Signs Regulations and General Directions throughout the duration of the temporary restriction. It is the Applicant's responsibility to ensure that appropriately accredited operators only are employed to place signage on the road network. It is the Applicant's responsibility to undertake initial liaison with bus companies, and other service organisations likely to be affected, and provide the Council with evidence of this.
+<p>It is the applicant’s responsibility to arrange, install, maintain and meet the cost of all on-site signing (including the diversion route where applicable; see Diversion Routes below) in accordance with Chapter 8 of the Traffic Signs Manual and / or Traffic Signs Regulations and General Directions throughout the duration of the temporary restriction. It is the Applicant's responsibility to ensure that appropriately accredited operators only are employed to place signage on the road network. It is the Applicant's responsibility to undertake initial liaison with bus companies, and other service organisations likely to be affected, and provide the Council with evidence of this.</p>
 
 
-Some useful telephone numbers are listed below, however other services may need to be contacted in addition.
+<p>Some useful telephone numbers are listed below, however other services may need to be contacted in addition.</p>
 
 
 
@@ -147,32 +153,32 @@ Prior warning to the public, via notices (in road stands) posted on all approach
 
 
 
-Diversion routes
+<h3>Diversion routes</h3>
 
 
-East Dunbartonshire Council has a network of traffic sensitive routes on which working times may be restricted, or where undue traffic disruption should be avoided. Our Neighbourhood Services Manager has a duty to limit the effect on traffic movement and may therefore impose conditions on diversions etc.
+<p>East Dunbartonshire Council has a network of traffic sensitive routes on which working times may be restricted, or where undue traffic disruption should be avoided. Our Neighbourhood Services Manager has a duty to limit the effect on traffic movement and may therefore impose conditions on diversions etc.</p>
 
 
-Diversions on the distributor road network will need to be approved by the Neighbourhood Services Manager.
+<p>Diversions on the distributor road network will need to be approved by the Neighbourhood Services Manager.</p>
 
 
-Supporting information required
+<h3>Supporting information required</h3>
 
 
 
-Plans at an appropriate scale illustrating the temporary restriction, diversion route and signing details (where applicable)
+<p> Plans at an appropriate scale illustrating the temporary restriction, diversion route and signing details (where applicable)</p>
 
-Fee (cheques made payable to East Dunbartonshire Council)
+<p> Fee (cheques made payable to East Dunbartonshire Council)</p>
 
-A copy of the applicants Public Liability Insurance with a minimum of £5,000,000 cover
+<p> A copy of the applicants Public Liability Insurance with a minimum of £5,000,000 cover</p>
 
-NB: Applicants should also be aware that they might require other licences from the Council, e.g. road opening, materials on the highway, craning, hoardings etc.
-
-
-Apply for a Temporary Road Closure or Temporary Parking Restriction
+<p> NB: Applicants should also be aware that they might require other licences from the Council, e.g. road opening, materials on the highway, craning, hoardings etc.</p>
 
 
-There are two ways of applying:
+<h3>Apply for a Temporary Road Closure or Temporary Parking Restriction</h3>
+
+
+<p>There are two ways of applying:</p>
 
 
 
