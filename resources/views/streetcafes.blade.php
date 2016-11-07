d4e7bb1323dbe72b8c34ba83b4a94a9a0fc8058b
SheepWaitForWolf <dglennie89@gmail.com>
2016-11-07 10:53:17 +0000

    street cafe blade update
    Fixed formatting and paragraphing using headers and line breaks

--- resources/views/streetcafes.blade.php
+++ resources/views/streetcafes.blade.php
@@ -8,52 +8,60 @@ Street Cafes
 <br>
 <div class="container">
 	<div class="col-md-8">
-		 <p>Road occupation consent
 
-If you wish to place tables and chairs on the road outside your premises then you must have consent from the Council.
+		<div class="col-md-12">
+			<h2> Street Cafes </h2>
+			<br>
+		</div>
 
-Depending on the type of change you wish to implement to your current licence, other licence applications may be required e.g. planning permission, liquor or food premises licence. Please contact the Council through Customer Services prior to completing your application.
+		 <h3>Road occupation consent </h3>
+
+<p> If you wish to place tables and chairs on the road outside your premises then you must have consent from the Council.</p>
+
+<p> Depending on the type of change you wish to implement to your current licence, other licence applications may be required e.g. planning permission, liquor or food premises licence. Please contact the Council through Customer Services prior to completing your application.</p>
 
 Fee
 
 For details of fees, please contact Customer Services (link sends e-mail).
 
-Eligibility Criteria
+<h3> Eligibility Criteria </h3>
 
 A summary of the eligibility criteria (link is external) for this licence.
 
-Regulation Summary
+<h3> Regulation Summary </h3>
 
 A summary of the regulations (link is external) for this licence.
 
-Application Evaluation Process
+<h3> Application Evaluation Process </h3>
 
 Consultation will be carried out with other Council Services to see if there are any associated issues e.g. licensing, planning and environmental health. If planning permission, a licence, or any other form of certification is deemed necessary, the applicant will be advised. A separate application should be made to the relevant Council service accompanied by the appropriate fee.
 
 Conditions may be attached to a consent.
 
-Will Tacit Consent Apply?
+<h3> Will Tacit Consent Apply? </h3>
 
-Yes. This means that you will be able to act as though your application is granted if you have not heard from the local authority by the end of the 120 day period permitted for final determination of the application.
-Apply for a permit to place table and chairs on the pavement or road
+<p> Yes. This means that you will be able to act as though your application is granted if you have not heard from the local authority by the end of the 120 day period permitted for final determination of the application.
+Apply for a permit to place table and chairs on the pavement or road </p>
 
 There are two ways of applying:
+<uL>
+<li> contact Customer Services (link sends e-mail)for an application form</li>
+<li>apply online Application for a permit to place tables and chairs on the pavement or road (link is external)</li>
+</ul>
 
-contact Customer Services (link sends e-mail)for an application form
-apply online Application for a permit to place tables and chairs on the pavement or road (link is external)
-For further information please contact Neighbourhood Services using the contact details provided.
+<p>For further information please contact Neighbourhood Services using the contact details provided.</p>
 
-Failed Application Redress
+<h3> Failed Application Redress </h3>
 
-Please contact the Council in the first instance.
+<p>Please contact the Council in the first instance.</p>
 
-Licence Holder Redress
+<h3> Licence Holder Redress </h3>
 
-Please contact the Council in the first instance.
+<p> Please contact the Council in the first instance.</p>
 
-Other Redress
+<h3> Other Redress </h3>
 
-Please contact the Council in the first instance about noise, pollution, etc or if one licence holder complains about another.
+<p>Please contact the Council in the first instance about noise, pollution, etc or if one licence holder complains about another.</p>
 
 Application to change a permit to place tables and chairs on the pavement or road
 
@@ -65,17 +73,18 @@ Fee
 
 For details of fees, please email us (link sends e-mail).
 
-Will Tacit Consent Apply?
+<h3> Will Tacit Consent Apply?</h3>
 
 Yes. This means that you will be able to act as though your application is granted if you have not heard from the local authority by the end of the 120 day period permitted for final determination of the application.
 
 Apply to change a permit to place tables and chairs on the pavement or road
 
 There are two ways of applying:
-
-contact Customer Services (link sends e-mail)
-apply online Application to change a permit to place tables and chairs on the pavement or road </p>
-
+<br>
+<ul>
+<li> contact Customer Services (link sends e-mail)</li>
+<li>apply online Application to change a permit to place tables and chairs on the pavement or road </p></li>
+</ul>
 		</div>
 </div>
 @stop
