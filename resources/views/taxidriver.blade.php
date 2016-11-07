9dfe7982da24b7fe89aa9f069628c91f4db7abe0
SheepWaitForWolf <dglennie89@gmail.com>
2016-11-07 09:42:27 +0000

    taxi driver blade added
    view for taxi driver license application added along with page content

---
+++ resources/views/taxidriver.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.master')
+
+@section('Title')
+Infographs
+@stop
+
+@section('Content')
+<br>
+<div class="container">
+	<div class="col-md-8">
+		 <p>The Council is responsible for the processing of all licence applications including Private Hire Car Drivers.
+
+In addition to the delegated powers to grant licences, the Council's Civic Government Appeals Board is the licensing authority for miscellaneous applications under the Civic Government (Scotland) Act 1982 which include the licensing of Private Hire Car Drivers.
+
+Payment Details
+
+A licence fee of £171 is payable for 3 years.
+
+How to Apply 
+
+The Taxi/Private Hire Car Driver Licence can be downloaded from our documents section.
+
+Please note you are required to provide the Council with a 'check code' (link is external) to allow your driving licence information to be shared as your paper counterpart of your licence is now no longer valid.
+
+Failed Application Redress
+
+If you wish to appeal against a decision you may do so to the local sheriff within 28 days of the decision. The sheriff can only uphold an appeal if they consider that the licensing authority:
+
+erred in law
+used incorrect material fact when making their decision
+acted contrary to natural justice
+didn't exercise their discretion in a reasonable manner
+Appeals against a sheriff's decision can be made to the Court of Session within 28 days of the decision.
+
+Licence Holder Redress
+
+If you wish to appeal against a decision you may do so to the local sheriff within 28 days of the decision. The sheriff can only uphold an appeal if they consider that the licensing authority:
+
+erred in law
+used incorrect material fact when making their decision
+acted contrary to natural justice
+didn't exercise their discretion in a reasonable manner
+Appeals against a sheriff's decision can be made to the Court of Session within 28 days of the decision.
+
+Consumer Complaint
+
+If you wish to appeal against a decision to grant a licence and you are a valid objector, you may do so to the local sheriff within 28 days of the decision. The sheriff can only uphold an appeal if they consider that the licensing authority:
+
+erred in law
+used incorrect material fact when making their decision
+acted contrary to natural justice
+didn't exercise their discretion in a reasonable manner
+Appeals against a sheriff's decision can be made to the Court of Session within 28 days of the decision.
+
+ </p>
+
+		</div>
+</div>
+@stop
