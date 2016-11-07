cdbabe2d748d3bfc783de8131d0d37aafef1742c
SheepWaitForWolf <dglennie89@gmail.com>
2016-11-07 10:02:52 +0000

    street cafes blade added
    view for street cafe license application added along with page content

---
+++ resources/views/streetcafes.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.master')
+
+@section('Title')
+Street Cafes
+@stop
+
+@section('Content')
+<br>
+<div class="container">
+	<div class="col-md-8">
+		 <p>Road occupation consent
+
+If you wish to place tables and chairs on the road outside your premises then you must have consent from the Council.
+
+Depending on the type of change you wish to implement to your current licence, other licence applications may be required e.g. planning permission, liquor or food premises licence. Please contact the Council through Customer Services prior to completing your application.
+
+Fee
+
+For details of fees, please contact Customer Services (link sends e-mail).
+
+Eligibility Criteria
+
+A summary of the eligibility criteria (link is external) for this licence.
+
+Regulation Summary
+
+A summary of the regulations (link is external) for this licence.
+
+Application Evaluation Process
+
+Consultation will be carried out with other Council Services to see if there are any associated issues e.g. licensing, planning and environmental health. If planning permission, a licence, or any other form of certification is deemed necessary, the applicant will be advised. A separate application should be made to the relevant Council service accompanied by the appropriate fee.
+
+Conditions may be attached to a consent.
+
+Will Tacit Consent Apply?
+
+Yes. This means that you will be able to act as though your application is granted if you have not heard from the local authority by the end of the 120 day period permitted for final determination of the application.
+Apply for a permit to place table and chairs on the pavement or road
+
+There are two ways of applying:
+
+contact Customer Services (link sends e-mail)for an application form
+apply online Application for a permit to place tables and chairs on the pavement or road (link is external)
+For further information please contact Neighbourhood Services using the contact details provided.
+
+Failed Application Redress
+
+Please contact the Council in the first instance.
+
+Licence Holder Redress
+
+Please contact the Council in the first instance.
+
+Other Redress
+
+Please contact the Council in the first instance about noise, pollution, etc or if one licence holder complains about another.
+
+Application to change a permit to place tables and chairs on the pavement or road
+
+If you wish to change or extend the area of road used for these purposes outside your premises then you must have consent from the Council.
+
+Depending on the type of change you wish to implement to your current licence, other licence applications may be required eg planning permission, liquor or food premises licence. Please contact us (link sends e-mail)prior to completing your application.
+
+Fee
+
+For details of fees, please email us (link sends e-mail).
+
+Will Tacit Consent Apply?
+
+Yes. This means that you will be able to act as though your application is granted if you have not heard from the local authority by the end of the 120 day period permitted for final determination of the application.
+
+Apply to change a permit to place tables and chairs on the pavement or road
+
+There are two ways of applying:
+
+contact Customer Services (link sends e-mail)
+apply online Application to change a permit to place tables and chairs on the pavement or road </p>
+
+		</div>
+</div>
+@stop
