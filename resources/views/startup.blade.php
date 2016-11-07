edb30986c02fbe06eeee25bc3239ad7452d3d190
SheepWaitForWolf <dglennie89@gmail.com>
2016-11-07 09:29:39 +0000

    start up advice update
    Added external links to page

--- resources/views/startup.blade.php
+++ resources/views/startup.blade.php
@@ -18,13 +18,13 @@ Help
 			<li> Business Finance </li>
 		</ul>
 		<p>
-We have experienced business advisors that can assist you through the process of starting and growing your business.  You can arrange an appointment with one of our Business Gateway (link is external) advisors in your local office (Kirkintilloch and Milngavie) by calling 0141 578 8530.
+We have experienced business advisors that can assist you through the process of starting and growing your business.  You can arrange an appointment with one of our <a href="http://www.bgateway.com/local-offices/east-dunbartonshire"> Business Gateway </a>(link is external) advisors in your local office (Kirkintilloch and Milngavie) by calling 0141 578 8530.
 
 New-start businesses are also able to access other forms of financial support:
 
-Start up Loans (link is external) can provide personal loans of between £1,000 and £25,000 to individuals that have recently started a new business. In addition to the loan, you will also be able to access support for up to 12 months from a skilled business advisor.
+<a href="https://www.startuploans.co.uk/">Start up Loans </a>(link is external) can provide personal loans of between £1,000 and £25,000 to individuals that have recently started a new business. In addition to the loan, you will also be able to access support for up to 12 months from a skilled business advisor.
 
-West of Scotland Loan Fund (link is external) can provide a business loan of between £20,000 to £100,000, up to 50% of a start-up finance package. This is only available to incorporated businesses.
+<a href="http://www.wslf.co.uk/">West of Scotland Loan Fund </a>(link is external) can provide a business loan of between £20,000 to £100,000, up to 50% of a start-up finance package. This is only available to incorporated businesses.
 
 There is also support for businesses that employ a young East Dunbartonshire resident in the form of a wage subsidy.
 	 	 </p>
