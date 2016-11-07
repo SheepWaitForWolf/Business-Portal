05efcf6e5879b0de8214c9eb5dff1c7f5b5756cc
SheepWaitForWolf <dglennie89@gmail.com>
2016-11-07 10:39:56 +0000

    operator blade update
    Fixed layout and paragraphing with headers and line breaks

--- resources/views/operator.blade.php
+++ resources/views/operator.blade.php
@@ -8,47 +8,59 @@ Taxi and Private Hire Operators
 <br>
 <div class="container">
 	<div class="col-md-8">
-		 <p> The Council is responsible for the processing of all licence applications including taxi operators.
 
-In addition to the delegated powers to grant licences, the Council's Civic Government Appeals Board is the licensing authority for miscellaneous applications under the Civic Government (Scotland) Act 1982 which include the licensing of taxi operators.
+		<div class="col-md-12">
+			<h2> Taxi and Private Hire Operators </h2>
+			<br>
+		</div>
+		 
+<p> The Council is responsible for the processing of all licence applications including taxi operators.
 
-Payment details
+<p> In addition to the delegated powers to grant licences, the Council's Civic Government Appeals Board is the licensing authority for miscellaneous applications under the Civic Government (Scotland) Act 1982 which include the licensing of taxi operators. </p>
 
-A licence fee of £365 for 3 years is payable.
+<h3> Payment details </h3>
 
-Apply for
+<p> A licence fee of £365 for 3 years is payable. </p>
 
-Please view attached forms.
+<h3> Apply for </h3>
 
-Failed application redress
+<p> Please view attached forms.</p>
+
+<h3> Failed application redress </h3>
+
+<p> If you wish to appeal against a decision you may do so to the local sheriff within 28 days of the decision. The sheriff can only uphold an appeal if they consider that the licensing authority: </p>
+
+<ul>
+<li>erred in law</li>
+<li>used incorrect material fact when making their decision</li>
+<li>acted contrary to natural justice</li>
+<li>didn't exercise their discretion in a reasonable manner</li>
+<li>Appeals against a sheriff's decision can be made to the Court of Session within 28 days of the decision.</li>
+</ul>
+
+<h3> Licence holder redress </h3>
 
 If you wish to appeal against a decision you may do so to the local sheriff within 28 days of the decision. The sheriff can only uphold an appeal if they consider that the licensing authority:
 
-erred in law
-used incorrect material fact when making their decision
-acted contrary to natural justice
-didn't exercise their discretion in a reasonable manner
-Appeals against a sheriff's decision can be made to the Court of Session within 28 days of the decision.
+<ul>
+<li>erred in law</li>
+<li>used incorrect material fact when making their decision</li>
+<li>acted contrary to natural justice</li>
+<li>didn't exercise their discretion in a reasonable manner</li>
+<li>Appeals against a sheriff's decision can be made to the Court of Session within 28 days of the decision.</li>
+</ul>
 
-Licence holder redress
+<h3> Consumer complaint </h3>
 
-If you wish to appeal against a decision you may do so to the local sheriff within 28 days of the decision. The sheriff can only uphold an appeal if they consider that the licensing authority:
+<p> If you wish to appeal against a decision to grant a licence and you are a valid objector, you may do so to the local sheriff within 28 days of the decision. The sheriff can only uphold an appeal if they consider that the licensing authority:</p>
 
-erred in law
-used incorrect material fact when making their decision
-acted contrary to natural justice
-didn't exercise their discretion in a reasonable manner
-Appeals against a sheriff's decision can be made to the Court of Session within 28 days of the decision.
-
-Consumer complaint
-
-If you wish to appeal against a decision to grant a licence and you are a valid objector, you may do so to the local sheriff within 28 days of the decision. The sheriff can only uphold an appeal if they consider that the licensing authority:
-
-erred in law
-used incorrect material fact when making their decision
-acted contrary to natural justice
-didn't exercise their discretion in a reasonable manner
-Appeals against a sheriff's decision can be made to the Court of Session within 28 days of the decision. </p>
+<ul>
+<li>erred in law</li>
+<li>used incorrect material fact when making their decision</li>
+<li>acted contrary to natural justice</li>
+<li>didn't exercise their discretion in a reasonable manner</li>
+<li>Appeals against a sheriff's decision can be made to the Court of Session within 28 days of the decision.</li>
+</ul>
 	</div>
 </div>
 @stop
